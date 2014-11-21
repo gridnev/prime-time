d2e41b9949fd9a6c1b22a5f22ce8878b8920809c
Denis Gridnev <denis.gridnev@gmail.com>
2014-11-21 18:52:30 +0300

    add some projects

---
+++ PrimeTime/PrimeTime.Services/KinohodCinemaService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PrimeTime.Common.Interfaces;
+
+namespace PrimeTime.Services
+{
+    public class KinohodCinemaService:ICinemaService
+    {
+        public void GetFilms()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
